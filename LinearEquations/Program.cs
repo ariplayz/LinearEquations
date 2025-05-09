05413c571b8478c969d3f215a8b21d64789cd156
Ari Cummings <ari@arigreenecummings.com>
2025-05-09 09:37:28 -0700

    Add

--- LinearEquations/Program.cs
+++ LinearEquations/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        num operation;
+        
+        Console.WriteLine("This is a calculator for Linear Equations in Algebra 1.");
+        Console.WriteLine("Select one of the following operations:");
+        Console.WriteLine("1. Take points and give equations, slope, and intercept.");
     }
 }
